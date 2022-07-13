36315aa9920184a6b0f17b9e3df59775c925a50c
CihanLoyan <cihanloyan@gmail.com>
2022-07-13 16:02:49 +0300

    definition of script tag and introduction to data types

---
+++ 03_dataTypes.js
@@ -0,0 +1,30 @@
+console.log(typeof 'JavaScript');
+console.log(typeof '12');
+console.log(typeof 4);
+console.log(typeof 4.6);
+console.log(typeof true);
+console.log(typeof TRUE);  // undefied
+console.log(typeof null);
+console.log(typeof undefined);
+
+/* 
+    Değişken isimlendirme kuralları:
+        Sayı ile başlayamaz.
+        $ ve _ dışında özel karakter içeremez.
+        camelCase tercih edilir.
+*/ 
+let _var1 = 'First variable'
+let $var_2 = 'Second variable'
+let $_var3 = 'Third variable'
+let _$var4 = 'Fourth variable'
+
+/* Aynı tipte değişkenleri alt alta şu şekilde yazabiliriz: */
+let firstName = "cihan",
+    age = 24,
+    isMarried = false
+    identity = undefined
+    job = null
+
+const GENDER = 'Male'
+
+console.log(firstName, age, isMarried, job, identity);
